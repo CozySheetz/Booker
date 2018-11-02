1aadce3d5b5e2a8b15318bd88f3b8d0a2b76d1f1
Kenny <kennethpolyak@gmail.com>
2018-11-02 12:20:09 -0400

    db connection cleanup for dev

--- database/index.js
+++ database/index.js
@@ -1,14 +1,5 @@
 var mysql = require('mysql');
 
-// set up connection
-// var connection = mysql.createConnection({
-// 	host     : 'booker.cp4l5wq0pfkx.us-east-2.rds.amazonaws.com',
-// 	port     : 3306,
-//   user     : 'kennethpolyak',
-//   password : 'Hrnyc18!',
-//   database : 'booker'
-// });
-
 var connection = mysql.createConnection({
 	host : 'localhost',
 	user : 'root',
