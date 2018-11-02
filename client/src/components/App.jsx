5be526668618149751e10c697e7cef4a5092058d
Kenny <kennethpolyak@gmail.com>
2018-11-02 16:12:33 -0400

    Adds log for debugging

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -79,7 +79,7 @@ class App extends React.Component {
 
 	fetchRawData() {
 		var id = window.location.pathname.slice(1)
-
+		console.log(window.location);
 		console.log('this id', id);
 
 		axios.get(`/listings/${id}`).then((res) => {
