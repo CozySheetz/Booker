54e512bec4f666692b4fa1eb1832993b635b348e
Kenny <kennethpolyak@gmail.com>
2018-11-04 15:09:12 -0500

    Update gitignore

--- database/configRDS.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-
-module.exports.connection = mysql.createConnection({
-  host     : 'booker.cp4l5wq0pfkx.us-east-2.rds.amazonaws.com',
-  port     : 3306,
-  user     : 'kennethpolyak',
-  password : 'Hrnyc18!',
-  database : 'booker'
-});
